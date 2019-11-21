1ffac8f706577e1a312495bca51b80638c326230
tianhan <63253294@qq.com>
2019-11-21 16:27:04 +0800

    add

--- chapter9.py
+++ chapter9.py
@@ -7,7 +7,7 @@ import os
 
 # to make this notebook's output stable across runs
 def reset_graph(seed=42):
-    tf.reset_default_graph()
+    # tf.reset_default_graph()
     tf.set_random_seed(seed)
     np.random.seed(seed)
 
@@ -36,3 +36,57 @@ reset_graph()
 x = tf.Variable(3, name="x")
 y = tf.Variable(4, name="y")
 f = x*x*y + y + 2
+
+# 实现线性回归
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+
+#reset_graph()
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+print(m,n)
+print(housing.target.reshape(-1, 1))
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+
+X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+XT = tf.transpose(X)
+print(XT)
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+
+with tf.Session() as sess:
+    theta_value = theta.eval()
+    print("线性回归 theta_value",theta_value)
+
+# 梯度下降
+reset_graph()
+
+n_epochs = 1000
+learning_rate = 0.01
+
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+gradients = tf.gradients(mse, [theta])[0]
+
+training_op = tf.assign(theta, theta - learning_rate * gradients)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+
+    best_theta = theta.eval()
+
+print("梯度下降 Best theta:")
+print(best_theta)
