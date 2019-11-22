aa6db0f3f7c49710ffaee97e970c83dd48aef94c
tianhan <63253294@qq.com>
2019-11-22 15:20:44 +0800

    1

--- chapter9.py
+++ chapter9.py
@@ -60,7 +60,7 @@ print("theta",theta)
 
 with tf.Session() as sess:
     theta_value = theta.eval()
-    print("线性回归 theta_value",theta_value)
+    print("线性回归 theta_value \r\n",theta_value)
 
 # 梯度下降
 reset_graph()
@@ -72,7 +72,7 @@ from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 scaled_housing_data = scaler.fit_transform(housing.data)
 scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
-print("归一化权重并附加常数1 计算前数据",scaled_housing_data_plus_bias)
+print("归一化权重并附加常数1 计算前数据\r\n",scaled_housing_data_plus_bias)
 
 X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
 y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
@@ -105,3 +105,38 @@ with tf.Session() as sess:
 
 print("梯度下降 Best theta:")
 print(best_theta)
+
+# 小批量梯度下降
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+n_epochs = 10
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)  # not shown in the book
+    indices = np.random.randint(m, size=batch_size)  # not shown
+    X_batch = scaled_housing_data_plus_bias[indices] # not shown
+    y_batch = housing.target.reshape(-1, 1)[indices] # not shown
+    return X_batch, y_batch
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+
+    best_theta = theta.eval()
+    print("小批量梯度下降\r\n",best_theta)
