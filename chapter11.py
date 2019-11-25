1c78b0a877c34158338c2686ba581d3d8fff21f3
tianhan <63253294@qq.com>
2019-11-25 18:07:01 +0800

    1

--- chapter11.py
+++ chapter11.py
@@ -232,6 +232,58 @@ saver = tf.train.Saver()
 n_epochs = 5
 batch_size = 50
 
+# with tf.Session() as sess:
+#     init.run()
+#     for epoch in range(n_epochs):
+#         for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+#             sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+#         accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+#         print(epoch, "Validation accuracy:", accuracy_val)
+#
+#     save_path = saver.save(sess, "./my_model_final.ckpt")
+
+# L1正则化
+reset_graph()
+
+n_inputs = 28 * 28  # MNIST
+n_hidden1 = 300
+n_outputs = 10
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+
+with tf.name_scope("dnn"):
+    hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.relu, name="hidden1")
+    logits = tf.layers.dense(hidden1, n_outputs, name="outputs")
+
+W1 = tf.get_default_graph().get_tensor_by_name("hidden1/kernel:0")
+W2 = tf.get_default_graph().get_tensor_by_name("outputs/kernel:0")
+
+scale = 0.001 # l1 regularization hyperparameter
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
+                                                              logits=logits)
+    base_loss = tf.reduce_mean(xentropy, name="avg_xentropy")
+    reg_losses = tf.reduce_sum(tf.abs(W1)) + tf.reduce_sum(tf.abs(W2))
+    loss = tf.add(base_loss, scale * reg_losses, name="loss")
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy")
+
+learning_rate = 0.01
+
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_epochs = 20
+batch_size = 200
+
 with tf.Session() as sess:
     init.run()
     for epoch in range(n_epochs):
