8ee85471705994b8651e8fe08422860f3366d0ea
tianhan <63253294@qq.com>
2019-11-25 16:12:48 +0800

    1

--- chapter11.py
+++ chapter11.py
@@ -85,5 +85,105 @@ with tf.Session() as sess:
 
     save_path = saver.save(sess, "./my_model_final.ckpt")
 
+# 应用批量归一化
+reset_graph()
 
+import tensorflow as tf
+
+n_inputs = 28 * 28
+n_hidden1 = 300
+n_hidden2 = 100
+n_outputs = 10
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+
+training = tf.placeholder_with_default(False, shape=(), name='training')
+
+hidden1 = tf.layers.dense(X, n_hidden1, name="hidden1")
+bn1 = tf.layers.batch_normalization(hidden1, training=training, momentum=0.9)
+bn1_act = tf.nn.elu(bn1)
+
+hidden2 = tf.layers.dense(bn1_act, n_hidden2, name="hidden2")
+bn2 = tf.layers.batch_normalization(hidden2, training=training, momentum=0.9)
+bn2_act = tf.nn.elu(bn2)
+
+logits_before_bn = tf.layers.dense(bn2_act, n_outputs, name="outputs")
+logits = tf.layers.batch_normalization(logits_before_bn, training=training,
+                                       momentum=0.9)
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+training = tf.placeholder_with_default(False, shape=(), name='training')
+
+from functools import partial
+
+my_batch_norm_layer = partial(tf.layers.batch_normalization,
+                              training=training, momentum=0.9)
+
+hidden1 = tf.layers.dense(X, n_hidden1, name="hidden1")
+bn1 = my_batch_norm_layer(hidden1)
+bn1_act = tf.nn.elu(bn1)
+hidden2 = tf.layers.dense(bn1_act, n_hidden2, name="hidden2")
+bn2 = my_batch_norm_layer(hidden2)
+bn2_act = tf.nn.elu(bn2)
+logits_before_bn = tf.layers.dense(bn2_act, n_outputs, name="outputs")
+logits = my_batch_norm_layer(logits_before_bn)
+
+reset_graph()
+
+batch_norm_momentum = 0.9
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+training = tf.placeholder_with_default(False, shape=(), name='training')
+
+with tf.name_scope("dnn"):
+    he_init = tf.variance_scaling_initializer()
+
+    my_batch_norm_layer = partial(
+        tf.layers.batch_normalization,
+        training=training,
+        momentum=batch_norm_momentum)
+
+    my_dense_layer = partial(
+        tf.layers.dense,
+        kernel_initializer=he_init)
+
+    hidden1 = my_dense_layer(X, n_hidden1, name="hidden1")
+    bn1 = tf.nn.elu(my_batch_norm_layer(hidden1))
+    hidden2 = my_dense_layer(bn1, n_hidden2, name="hidden2")
+    bn2 = tf.nn.elu(my_batch_norm_layer(hidden2))
+    logits_before_bn = my_dense_layer(bn2, n_outputs, name="outputs")
+    logits = my_batch_norm_layer(logits_before_bn)
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_epochs = 20
+batch_size = 200
+
+extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+
+with tf.Session() as sess:
+    init.run()
+    for epoch in range(n_epochs):
+        for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+            sess.run([training_op, extra_update_ops],
+                     feed_dict={training: True, X: X_batch, y: y_batch})
+        accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+        print(epoch, "Validation accuracy:", accuracy_val)
+
+    save_path = saver.save(sess, "./my_model_final.ckpt")
 
