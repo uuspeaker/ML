ac0f1a4b8aa89ea9f39b225269142806c6ddd67d
tianhan <63253294@qq.com>
2019-11-25 11:22:02 +0800

    1

--- chapter11.py
+++ chapter11.py
@@ -12,7 +12,7 @@ reset_graph()
 n_inputs = 28 * 28  # MNIST
 n_hidden1 = 300
 
-X = tf.keras.backend.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+X = tf.compat.v1.placeholder(tf.float32, shape=(None, n_inputs), name="X")
 he_init = tf.variance_scaling_initializer()
 hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.relu,
                           kernel_initializer=he_init, name="hidden1")
