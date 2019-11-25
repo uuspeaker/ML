cb4bba81d8e470b2fe468b24b5c72bb83e21d5c1
tianhan <63253294@qq.com>
2019-11-25 18:19:41 +0800

    1

--- chapter11.py
+++ chapter11.py
@@ -252,22 +252,24 @@ n_outputs = 10
 X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
 y = tf.placeholder(tf.int32, shape=(None), name="y")
 
-with tf.name_scope("dnn"):
-    hidden1 = tf.layers.dense(X, n_hidden1, activation=tf.nn.relu, name="hidden1")
-    logits = tf.layers.dense(hidden1, n_outputs, name="outputs")
-
-W1 = tf.get_default_graph().get_tensor_by_name("hidden1/kernel:0")
-W2 = tf.get_default_graph().get_tensor_by_name("outputs/kernel:0")
-print("W1,W2", W1, W2)
-
 scale = 0.001 # l1 regularization hyperparameter
 
-with tf.name_scope("loss"):
-    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,
-                                                              logits=logits)
-    base_loss = tf.reduce_mean(xentropy, name="avg_xentropy")
-    reg_losses = tf.reduce_sum(tf.abs(W1)) + tf.reduce_sum(tf.abs(W2))
-    loss = tf.add(base_loss, scale * reg_losses, name="loss")
+my_dense_layer = partial(
+    tf.layers.dense, activation=tf.nn.relu,
+    kernel_regularizer=tf.contrib.layers.l1_regularizer(scale))
+
+with tf.name_scope("dnn"):
+    hidden1 = my_dense_layer(X, n_hidden1, name="hidden1")
+    hidden2 = my_dense_layer(hidden1, n_hidden2, name="hidden2")
+    logits = my_dense_layer(hidden2, n_outputs, activation=None,
+                            name="outputs")
+
+with tf.name_scope("loss"):                                     # not shown in the book
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(  # not shown
+        labels=y, logits=logits)                                # not shown
+    base_loss = tf.reduce_mean(xentropy, name="avg_xentropy")   # not shown
+    reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    loss = tf.add_n([base_loss] + reg_losses, name="loss")
 
 with tf.name_scope("eval"):
     correct = tf.nn.in_top_k(logits, y, 1)
