4241466727b013a197b037110ae0d1effca9551d
tianhan <63253294@qq.com>
2019-11-26 09:41:04 +0800

    1

--- chapter11.py
+++ chapter11.py
@@ -287,13 +287,62 @@ saver = tf.train.Saver()
 n_epochs = 20
 batch_size = 200
 
+# with tf.Session() as sess:
+#     init.run()
+#     for epoch in range(n_epochs):
+#         for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+#             sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+#         accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+#         print(epoch, "Validation accuracy:", accuracy_val)
+#
+#     save_path = saver.save(sess, "./my_model_final.ckpt")
+
+# 使用dropout
+
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=(None, n_inputs), name="X")
+y = tf.placeholder(tf.int32, shape=(None), name="y")
+
+training = tf.placeholder_with_default(False, shape=(), name='training')
+
+dropout_rate = 0.5  # == 1 - keep_prob
+X_drop = tf.layers.dropout(X, dropout_rate, training=training)
+
+with tf.name_scope("dnn"):
+    hidden1 = tf.layers.dense(X_drop, n_hidden1, activation=tf.nn.relu,
+                              name="hidden1")
+    hidden1_drop = tf.layers.dropout(hidden1, dropout_rate, training=training)
+    hidden2 = tf.layers.dense(hidden1_drop, n_hidden2, activation=tf.nn.relu,
+                              name="hidden2")
+    hidden2_drop = tf.layers.dropout(hidden2, dropout_rate, training=training)
+    logits = tf.layers.dense(hidden2_drop, n_outputs, name="outputs")
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.reduce_mean(xentropy, name="loss")
+
+with tf.name_scope("train"):
+    optimizer = tf.train.MomentumOptimizer(learning_rate, momentum=0.9)
+    training_op = optimizer.minimize(loss)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_epochs = 20
+batch_size = 50
+
 with tf.Session() as sess:
     init.run()
     for epoch in range(n_epochs):
         for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
-            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch, training: True})
         accuracy_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
         print(epoch, "Validation accuracy:", accuracy_val)
 
     save_path = saver.save(sess, "./my_model_final.ckpt")
-
