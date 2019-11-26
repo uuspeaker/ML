859b3ffc8b7174dc0ce1a21c9f033be0a84967ac
tianhan <63253294@qq.com>
2019-11-26 15:07:37 +0800

    1

--- chapter13.py
+++ chapter13.py
@@ -50,6 +50,36 @@ convolution = tf.nn.conv2d(X, feature_maps, strides=[1,1,1,1], padding="SAME")
 # save_fig("china_original", tight_layout=False)
 # plt.show()
 
+# 卷基层案例
+import numpy as np
+from sklearn.datasets import load_sample_images
+
+# Load sample images
+china = load_sample_image("china.jpg")
+flower = load_sample_image("flower.jpg")
+dataset = np.array([china, flower], dtype=np.float32)
+batch_size, height, width, channels = dataset.shape
+
+# Create 2 filters
+filters = np.zeros(shape=(7, 7, channels, 2), dtype=np.float32)
+filters[:, 3, :, 0] = 1  # vertical line
+filters[3, :, :, 1] = 1  # horizontal line
+
+# Create a graph with input X plus a convolutional layer applying the 2 filters
+X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+convolution = tf.nn.conv2d(X, filters, strides=[1,2,2,1], padding="SAME")
+
+with tf.Session() as sess:
+    output = sess.run(convolution, feed_dict={X: dataset})
+
+plt.imshow(output[0, :, :, 1], cmap="gray") # plot 1st image's 2nd feature map
+plt.show()
+
+for image_index in (0, 1):
+    for feature_map_index in (0, 1):
+        plot_image(output[image_index, :, :, feature_map_index])
+        plt.show()
+
 # 使用tf.layers.conv2d()
 reset_graph()
 
@@ -64,3 +94,5 @@ with tf.Session() as sess:
 
 plt.imshow(output[0, :, :, 1], cmap="gray") # plot 1st image's 2nd feature map
 plt.show()
+
+
