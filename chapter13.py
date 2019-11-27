fe97eb0ef158eba4940f5aebe4abc706fc9f86a0
tianhan <63253294@qq.com>
2019-11-27 10:29:13 +0800

    1

--- chapter13.py
+++ chapter13.py
@@ -196,7 +196,8 @@ with tf.Session() as sess:
         num = 0
         for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
             num += 1
-            print("epoch num", epoch, num)
+            print("epoch num X_batch.shape", epoch, num, X_batch.shape)
+
             sess.run(training_op, feed_dict={X: X_batch, y: y_batch},options = run_opts)
         acc_batch = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
         acc_test = accuracy.eval(feed_dict={X: X_test, y: y_test})
