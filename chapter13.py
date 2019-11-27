f846e987bfcc496aa7a582841a41fb0c132cd246
tianhan <63253294@qq.com>
2019-11-27 15:14:42 +0800

    1

--- chapter13.py
+++ chapter13.py
@@ -176,7 +176,7 @@ with tf.name_scope("init_and_save"):
     saver = tf.train.Saver()
 
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
-shuffle_index = np.random.permutation(60000)
+shuffle_index = np.random.permutation(6000)
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
 print('X_train.shape,y_train.shape========', X_train.shape,y_train.shape)
 print('X_test.shape,y_test.shape========', X_test.shape,y_test.shape)
@@ -195,26 +195,140 @@ def shuffle_batch(X, y, batch_size):
         X_batch, y_batch = X[batch_idx], y[batch_idx]
         yield X_batch, y_batch
 
-n_epochs = 2
+n_epochs = 1
 batch_size = 100
 
 run_opts = tf.RunOptions(report_tensor_allocations_upon_oom = True)
+# with tf.Session() as sess:
+#     init.run()
+#     for epoch in range(n_epochs):
+#         num = 0
+#
+#         for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+#             num += 1
+#
+#             # print("epoch num X_batch.shape y_batch.shape", epoch, num, X_batch.shape, y_batch.shape)
+#
+#             sess.run(training_op, feed_dict={X: X_batch, y: y_batch},options = run_opts)
+#         acc_batch = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
+#         acc_test = accuracy.eval(feed_dict={X: X_test[:1000], y: y_test[:1000]})
+#         print(epoch, "Last batch accuracy:", acc_batch, "Test accuracy:", acc_test)
+#
+#         save_path = saver.save(sess, "./my_mnist_model")
+
+
+# 训练带有dropout的网络
+height = 28
+width = 28
+channels = 1
+n_inputs = height * width
+
+conv1_fmaps = 32
+conv1_ksize = 3
+conv1_stride = 1
+conv1_pad = "SAME"
+
+conv2_fmaps = 64
+conv2_ksize = 3
+conv2_stride = 1
+conv2_pad = "SAME"
+conv2_dropout_rate = 0.25
+
+pool3_fmaps = conv2_fmaps
+
+n_fc1 = 128
+fc1_dropout_rate = 0.5
+
+n_outputs = 10
+
+reset_graph()
+
+with tf.name_scope("inputs"):
+    X = tf.placeholder(tf.float32, shape=[None, n_inputs], name="X")
+    X_reshaped = tf.reshape(X, shape=[-1, height, width, channels])
+    y = tf.placeholder(tf.int32, shape=[None], name="y")
+    training = tf.placeholder_with_default(False, shape=[], name='training')
+
+conv1 = tf.layers.conv2d(X_reshaped, filters=conv1_fmaps, kernel_size=conv1_ksize,
+                         strides=conv1_stride, padding=conv1_pad,
+                         activation=tf.nn.relu, name="conv1")
+conv2 = tf.layers.conv2d(conv1, filters=conv2_fmaps, kernel_size=conv2_ksize,
+                         strides=conv2_stride, padding=conv2_pad,
+                         activation=tf.nn.relu, name="conv2")
+
+with tf.name_scope("pool3"):
+    pool3 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
+    pool3_flat = tf.reshape(pool3, shape=[-1, pool3_fmaps * 14 * 14])
+    pool3_flat_drop = tf.layers.dropout(pool3_flat, conv2_dropout_rate, training=training)
+
+with tf.name_scope("fc1"):
+    fc1 = tf.layers.dense(pool3_flat_drop, n_fc1, activation=tf.nn.relu, name="fc1")
+    fc1_drop = tf.layers.dropout(fc1, fc1_dropout_rate, training=training)
+
+with tf.name_scope("output"):
+    logits = tf.layers.dense(fc1_drop, n_outputs, name="output")
+    Y_proba = tf.nn.softmax(logits, name="Y_proba")
+
+with tf.name_scope("train"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+    loss = tf.reduce_mean(xentropy)
+    optimizer = tf.train.AdamOptimizer()
+    training_op = optimizer.minimize(loss)
+
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits, y, 1)
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+with tf.name_scope("init_and_save"):
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    
+def get_model_params():
+    gvars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
+    return {gvar.op.name: value for gvar, value in zip(gvars, tf.get_default_session().run(gvars))}
+
+def restore_model_params(model_params):
+    gvar_names = list(model_params.keys())
+    assign_ops = {gvar_name: tf.get_default_graph().get_operation_by_name(gvar_name + "/Assign")
+                  for gvar_name in gvar_names}
+    init_values = {gvar_name: assign_op.inputs[1] for gvar_name, assign_op in assign_ops.items()}
+    feed_dict = {init_values[gvar_name]: model_params[gvar_name] for gvar_name in gvar_names}
+    tf.get_default_session().run(assign_ops, feed_dict=feed_dict)
+
+n_epochs = 1000
+batch_size = 50
+iteration = 0
+
+best_loss_val = np.infty
+check_interval = 500
+checks_since_last_progress = 0
+max_checks_without_progress = 20
+best_model_params = None
+
 with tf.Session() as sess:
     init.run()
     for epoch in range(n_epochs):
-        num = 0
-
         for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
-            num += 1
-
-            # print("epoch num X_batch.shape y_batch.shape", epoch, num, X_batch.shape, y_batch.shape)
-
-            sess.run(training_op, feed_dict={X: X_batch, y: y_batch},options = run_opts)
+            iteration += 1
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch, training: True})
+            if iteration % check_interval == 0:
+                loss_val = loss.eval(feed_dict={X: X_valid, y: y_valid})
+                if loss_val < best_loss_val:
+                    best_loss_val = loss_val
+                    checks_since_last_progress = 0
+                    best_model_params = get_model_params()
+                else:
+                    checks_since_last_progress += 1
         acc_batch = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
-        acc_test = accuracy.eval(feed_dict={X: X_test[:1000], y: y_test[:1000]})
-        print(epoch, "Last batch accuracy:", acc_batch, "Test accuracy:", acc_test)
+        acc_val = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
+        print("Epoch {}, last batch accuracy: {:.4f}%, valid. accuracy: {:.4f}%, valid. best loss: {:.6f}".format(
+                  epoch, acc_batch * 100, acc_val * 100, best_loss_val))
+        if checks_since_last_progress > max_checks_without_progress:
+            print("Early stopping!")
+            break
 
-        save_path = saver.save(sess, "./my_mnist_model")
-
-
-
+    if best_model_params:
+        restore_model_params(best_model_params)
+    acc_test = accuracy.eval(feed_dict={X: X_test, y: y_test})
+    print("Final accuracy on test set:", acc_test)
+    save_path = saver.save(sess, "./my_mnist_model")
