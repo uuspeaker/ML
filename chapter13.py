32fd0e8f69db8c31c7a5bc35e53876a20828b540
tianhan <63253294@qq.com>
2019-11-27 10:32:35 +0800

    1

--- chapter13.py
+++ chapter13.py
@@ -140,20 +140,26 @@ with tf.name_scope("inputs"):
 conv1 = tf.layers.conv2d(X_reshaped, filters=conv1_fmaps, kernel_size=conv1_ksize,
                          strides=conv1_stride, padding=conv1_pad,
                          activation=tf.nn.relu, name="conv1")
+print("conv1.shape", conv1.shape)
 conv2 = tf.layers.conv2d(conv1, filters=conv2_fmaps, kernel_size=conv2_ksize,
                          strides=conv2_stride, padding=conv2_pad,
                          activation=tf.nn.relu, name="conv2")
-
+print("conv2.shape", conv2.shape)
 with tf.name_scope("pool3"):
     pool3 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="VALID")
+    print("pool3.shape", pool3.shape)
     pool3_flat = tf.reshape(pool3, shape=[-1, pool3_fmaps * 7 * 7])
+    print("pool3_flat.shape", pool3_flat.shape)
 
 with tf.name_scope("fc1"):
     fc1 = tf.layers.dense(pool3_flat, n_fc1, activation=tf.nn.relu, name="fc1")
+    print("fc1.shape", fc1.shape)
 
 with tf.name_scope("output"):
     logits = tf.layers.dense(fc1, n_outputs, name="output")
+    print("logits.shape", logits.shape)
     Y_proba = tf.nn.softmax(logits, name="Y_proba")
+    print("Y_proba.shape", Y_proba.shape)
 
 with tf.name_scope("train"):
     xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
