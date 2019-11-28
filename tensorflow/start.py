19b0b7026e182eb6c73f7238875be8d716e33bb6
tianhan <63253294@qq.com>
2019-11-28 15:08:10 +0800

    1

--- tensorflow/start.py
+++ tensorflow/start.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-(x_train, y_train) = (x_train[:1000], y_train[:1000])
+(x_train, y_train) = (x_train[:60000], y_train[:60000])
 (x_test, y_test) = (x_test[:1000], y_test[:1000])
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
@@ -22,6 +22,15 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+batch_size = 100
+def shuffle_batch(X, y, batch_size):
+    rnd_idx = np.random.permutation(len(X))
+    n_batches = len(X) // batch_size
+    for batch_idx in np.array_split(rnd_idx, n_batches):
+        X_batch, y_batch = X[batch_idx], y[batch_idx]
+        yield X_batch, y_batch
+
+for X_batch, y_batch in shuffle_batch(X_train, y_train, batch_size):
+    model.fit(X_batch, y_batch, epochs=5)
 
 model.evaluate(x_test,  y_test, verbose=2)
