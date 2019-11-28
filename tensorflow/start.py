a6409f3e6c51857c22a61eeb65f8faaa12364160
tianhan <63253294@qq.com>
2019-11-28 15:03:16 +0800

    1

--- tensorflow/start.py
+++ tensorflow/start.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train) = (x_train[:1000], y_train[:1000])
+(x_test, y_test) = (x_test[:1000], y_test[:1000])
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 model = tf.keras.models.Sequential([
