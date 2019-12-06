1765e0da3259b681c484106bebf70981d52378e7
tianhan <63253294@qq.com>
2019-12-06 13:53:18 +0800

    1

---
+++ chapter16.py
@@ -0,0 +1,98 @@
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+
+# Common imports
+import numpy as np
+import os
+import sys
+
+# to make this notebook's output stable across runs
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+
+# To plot pretty figures and animations
+import matplotlib
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "rl"
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+
+import gym
+env = gym.make('MsPacman-v0')
+obs = env.reset()
+print("obs.shape",obs.shape)
+img = env.render(mode="rgb_array")
+plt.figure(figsize=(5,4))
+plt.imshow(img)
+plt.axis("off")
+save_fig("MsPacman")
+plt.show()
+
+(img == obs).all()
+
+def plot_environment(env, figsize=(5,4)):
+    plt.close()  # or else nbagg sometimes plots in the previous cell
+    plt.figure(figsize=figsize)
+    img = env.render(mode="rgb_array")
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
+
+print("env.action_space",env.action_space)
+env.reset()
+for step in range(110):
+    env.step(3) #left
+for step in range(40):
+    env.step(8) #lower-left
+
+plot_environment(env)
+
+obs, reward, done, info = env.step(0)
+
+print("obs.shape",obs.shape)
+print("obs, reward, done, info",obs, reward, done, info)
+
+frames = []
+
+n_max_steps = 1000
+n_change_steps = 10
+
+obs = env.reset()
+for step in range(n_max_steps):
+    img = env.render(mode="rgb_array")
+    frames.append(img)
+    if step % n_change_steps == 0:
+        action = env.action_space.sample() # play randomly
+    obs, reward, done, info = env.step(action)
+    if done:
+        break
+
+def update_scene(num, frames, patch):
+    patch.set_data(frames[num])
+    return patch,
+
+def plot_animation(frames, repeat=False, interval=40):
+    plt.close()  # or else nbagg sometimes plots in the previous cell
+    fig = plt.figure()
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+    return animation.FuncAnimation(fig, update_scene, fargs=(frames, patch), frames=len(frames), repeat=repeat, interval=interval)
+
+video = plot_animation(frames)
+plt.show()
+
+
